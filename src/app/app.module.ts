4201ef468d8ccc47bfd141d5336c0aa05fa66cb7
Irving Martínez <irving.mtqz10@yahoo.com>
2022-02-06 23:39:57 -0600

    Petición fetch y presentando datos por console

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -12,7 +13,8 @@ import { TableComponent } from './table/table.component';
     TableComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'table-component',
   templateUrl: './table.component.html',
   styleUrls: ['table.component.css']
 })
-export class TableComponent{
+export class TableComponent implements OnInit{
   titles: String[] = [
     '#',
     'Coin',
@@ -13,5 +14,18 @@ export class TableComponent{
     'Price Change',
     '24h Volume'
   ]
+
+  constructor(private http: HttpClient){}
   
+  ngOnInit(): void {
+    this.http.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+      .subscribe(
+        res => {
+          console.log(res);
+        },
+        err => {
+          console.log(err);
+        }
+      )
+  }
 }
